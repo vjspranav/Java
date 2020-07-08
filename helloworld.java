4b1931a1118db81bb56ff83532ac335517b5d3c7
Rohan Hasabe <rohanhasabe8@gmail.com>
2020-07-08 12:44:06 +0530

    Day 0

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld //Class name mostly should be according to file name
+    {
+        public static void main(String args[])
+        {
+            System.out.println("Hello World");
+        }
+    }
