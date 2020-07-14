be61056281bcb1716a7853e15c5406632e059fce
Rohan Hasabe <rohanhasabe8@gmail.com>
2020-07-15 01:58:17 +0530

    class and objects

---
+++ class_objects.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Student
+{
+    int roll;
+    String name;
+    float percentage;
+}
+public class class_objects
+{
+    public static void main(String[] args) 
+    {
+    Student s1 = new Student();
+    s1.roll = 04;
+    s1.name = "Jack";
+    s1.percentage = 92.0f;
+    System.out.println("Student's Roll-Number is :- "+ s1.roll);
+    System.out.println(s1.name);
+    System.out.println(s1.percentage);
+    Student s2 = new Student();
+    s2.roll = 44;
+    s2.name = "Sam";
+    s2.percentage = 91.3f;
+    System.out.println(s2.roll);
+    System.out.println(s2.name);
+    System.out.println(s2.percentage);
+    }
+}
---
+++ sumof2num.java
@@ -0,0 +1,13 @@
+public class sumof2num 
+{
+    public static void main(final String[] args)
+    {
+        int num1,num2,sum;
+        num1=1000;
+        num2=2000;
+        double d1 = 2.989845649464647d;
+        sum= num1 + num2;
+        System.out.println("Sum is:-"+ sum);
+        System.out.println(d1);
+    }
+}
