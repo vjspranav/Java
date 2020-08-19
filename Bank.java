086b49f0b96ef472ea37ca3fd269af3a77df698a
Rohan Hasabe <rohanhasabe8@gmail.com>
2020-08-20 04:33:35 +0530

    Final changes; Assignment 3 OOP

--- Bank.java
+++ Bank.java
@@ -7,11 +7,21 @@ class Accounts
 	private double balance;
 	private String name;
 	private int accno;
+	private double initialBalance;
+	private double amount;
+	private double deposit;
+	private double withdraw;
 
 	public void name()
 	{
-		System.out.print("Enter Name: ");
+		System.out.print("Enter Name:- ");
         name = sc.next();
+	}
+
+	public void accno()
+	{
+		System.out.print("Enter Account Number:- ");
+        accno = sc.nextInt();
 	}
 
 	public Accounts()
@@ -19,24 +29,30 @@ class Accounts
 		balance = 0;
 	}
 
-	public Accounts(double initialBalance)
+	public void balance()
 	{
+		System.out.print("First time money deposit:- ");
+		initialBalance = sc.nextDouble();
 		balance = initialBalance;
 	}
 
-	public void deposit(double amount)
+	public void deposit()
 	{
-		balance = balance + amount;
+		System.out.print("Enter the amount you want to deposit:- ");
+        deposit = sc.nextDouble();
+		balance = balance + deposit;
 	}
 
-	public void withdraw(double amount)
+	public void withdraw()
 	{
-		balance = balance - amount;
+		System.out.print("Enter the amount you want to withdraw:- ");
+        withdraw = sc.nextDouble();
+		balance = balance - withdraw;
 	}
 
-	public double getBalance()
+	public void getBalance()
 	{
-		return balance;
+		System.out.println(name+", Your account balance is:- "+balance);
 	}
 
 }
@@ -44,10 +60,12 @@ public class Bank
 {
 	public static void main(String [] args)
 	{
-		Accounts p1Savings = new Accounts(1000);
+		Accounts p1Savings = new Accounts();
 		p1Savings.name();
-		p1Savings.withdraw(250);
-		p1Savings.deposit(400);
-		System.out.println(p1Savings.name+"The account balance is:- "+p1Savings.getBalance());
+		p1Savings.accno();
+		p1Savings.balance();
+		p1Savings.withdraw();
+		p1Savings.deposit();
+		p1Savings.getBalance();
 	}
 }
