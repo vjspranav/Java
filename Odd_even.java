707bcec9b01e121f183c4a8bf2aa49680c74cb45
Vjs Pranav <pranavasri@live.in>
2020-09-30 21:00:52 +0530

    Refactor Code
    Looks Cleaner

--- Odd_even.java
+++ Odd_even.java
@@ -1,20 +1,16 @@
+  GNU nano 5.2                                                                                                    Odd_even.java                                                                                                     Modified  
 import java.util.Scanner;
-public class Odd_even 
+public class Odd_even
 {
     public static void main(final String[] args)
-    {   
+    {
         // User Input
         Scanner num1 = new Scanner(System.in);
-        System.out.println("Enter a number :- ");
+        System.out.print("Enter a number :- ");
         int a = num1.nextInt();
-        if (a % 2 == 0)
-        {
-            System.out.println("Number is even");
-        }
-        else
-        {
-            System.out.println("Number is odd");
-        }
+        String oe=(a%2==0)?"Even":"Odd";
+        System.out.println("Number is " + oe);
         num1.close();
-    }    
+    }
 }
+
